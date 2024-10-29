4770eab72a4caf84dfa9d7dfb8099b904713438b
MarcusPizzimenti <MarcusPizzimenti>
2024-10-29 00:15:34 -0300

    upload do projeto atualizado

--- Seminario.py
+++ Seminario.py
@@ -5,6 +5,11 @@ import pandas as pd
 # Definir PRNT aqui, por exemplo:
 PRNT = 90  # Ajuste conforme necessário
 
+# Custos
+custo_cal = 125  # Custo por tonelada de calcario calcitico, em reais (2024)
+custo_ges = 140  # Custo por tonelada de gesso agricola, em reais (2024)
+
+
 # Funções para calcular a necessidade de calagem e gessagem
 def calcular_calagem(V1, V2, CTC, area):
     # Cálculo da necessidade de calagem
@@ -25,6 +30,7 @@ df['Ca'] = pd.to_numeric(df['Ca'], errors='coerce')
 df['V'] = pd.to_numeric(df['V'], errors='coerce')
 df['CTC'] = pd.to_numeric(df['CTC'], errors='coerce')
 df['Área talhão'] = pd.to_numeric(df['Área talhão'], errors='coerce')
+df['Pontos'] = pd.to_numeric(df['Pontos'], errors='coerce')
 
 # Lista para armazenar resultados
 resultados = []
@@ -38,7 +44,7 @@ for index, row in df.iterrows():
     m = row["m"]
     Ca = row["Ca"]
     profundidade = row["Profun"]  # Supondo que esta seja a coluna com profundidade
- 
+    Pontos = row["Pontos"]
     
     NG = 0
     NC = 0
@@ -52,12 +58,17 @@ for index, row in df.iterrows():
         if m > 20 or Ca < 5 or V1 < 35:  # Condições para gessagem
             NG = calcular_gessagem(V1, V2, CTC, area)
 
+      # Cálculo de custos
+    custo_total_calagem = NC * custo_cal
+    custo_total_gessagem = NG * custo_ges
+
+
     # Adicionando resultados
-    resultados.append({"Cultura": row["Cultura"], "Tipo": "Calagem", "Área talhão": area, "Quantidade (t)": NC})
-    resultados.append({"Cultura": row["Cultura"], "Tipo": "Gessagem", "Área talhão": area, "Quantidade (t)": NG})
+    resultados.append({"Pontos": row["Pontos"], "Cultura": row["Cultura"], "Tipo": "Calagem", "Área talhão": area, "Quantidade (t)": NC, "Custo Total (R$)": custo_total_calagem})
+    resultados.append({"Pontos": row["Pontos"], "Cultura": row["Cultura"], "Tipo": "Gessagem", "Área talhão": area, "Quantidade (t)": NG, "Custo Total (R$)": custo_total_gessagem})
 
 # Criando um DataFrame com os resultados
-resultados_df = pd.DataFrame(resultados, columns=["Cultura", "Tipo", "Área talhão", "Quantidade (t)"])
+resultados_df = pd.DataFrame(resultados, columns=["Pontos", "Cultura", "Tipo", "Área talhão", "Quantidade (t)", "Custo Total (R$)"])
 
 # Salvando os resultados em um arquivo CSV
 resultados_df.to_csv(r'/home/marcus_pizzi/Documents/resultados.csv', index=False)
